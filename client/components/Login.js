e7eb015c60266279860ebd48f8103422160d0029
BildadOwuor <bildadowuor@gmail.com>
2022-12-23 11:43:44 +0300

    initial commit

---
+++ client/components/Login.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-routr-dom";
+
+
+const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        if (username.trim() && password.trim()) {
+            e.preventDefault();
+            console.log({ username, password });
+            setPassword("");
+            setUsername("");
+        }
+    }
+
+    return (
+        <main className='login'>
+            <form className="login_form" onSubmit={handleSubmit}>
+                <h2 className="login_title"> Log into your account</h2>
+                <label htmlFor="username">User Name</label>
+                <input
+                    id="username"
+                    name="username"
+                    type='text'
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    className='username'
+                />
+                <label htmlFor="password">Password</label>
+                <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="password"
+                />
+                <button className="loginButton">LOG IN</button>
+                <p style={{ textAlign: "center", margibTop: '30px' }}>
+                    Don't have an account? {" "}
+                    <Link className='link' to="/register">
+                        Create one
+                    </Link>
+
+                </p>
+                
+            </form>
+
+        </main>
+
+    )
+}
