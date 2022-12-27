c6135fe6523083c8e0e0fcc502b4a61f85fb68c4
BildadOwuor <bildadowuor@gmail.com>
2022-12-27 15:24:08 +0300

    fixed CORS error

--- client/src/utils/resource.js
+++ client/src/utils/resource.js
@@ -2,38 +2,43 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 export const time = [
-    { id: "null", t: "Select" },
-    { id: "7", t: "7:00am" },
-    { id: "8", t: "8:00am" },
-    { id: "9", t: "9:00am" },
-    { id: "10", t: "10:00am" },
-    { id: "11", t: "11:00am" },
-    { id: "12", t: "12:00am" },
-    { id: "13", t: "13:00pm" },
-    { id: "14", t: "14:00pm" },
-    { id: "15", t: "15:00pm" },
-    { id: "16", t: "16:00pm" },
-    { id: "17", t: "17:00pm" },
-    { id: "18", t: "18:00pm" },
-    { id: "19", t: "19:00pm" }
-    
+  { id: "null", t: "Select" },
+  { id: "7", t: "7:00am" },
+  { id: "8", t: "8:00am" },
+  { id: "9", t: "9:00am" },
+  { id: "10", t: "10:00am" },
+  { id: "11", t: "11:00am" },
+  { id: "12", t: "12:00am" },
+  { id: "13", t: "13:00pm" },
+  { id: "14", t: "14:00pm" },
+  { id: "15", t: "15:00pm" },
+  { id: "16", t: "16:00pm" },
+  { id: "17", t: "17:00pm" },
+  { id: "18", t: "18:00pm" },
+  { id: "19", t: "19:00pm" },
 ];
-
 
 //register new user and store the information in the server.
 export async function handleRegister(email, username, password, navigate) {
-    
-
-    axios.post("http://localhost:4000/register", { email, username, password }, {headers:{
-        "Content-Type":"application/json",
-        Accept: "application/json",
-    },},).then(res => {
-        console.log("Response received: ", res);
-        console.log(res.data);
-         toast.success(res);
-        navigate("/dashboard");
-    }).catch(err => {
-        console.log("Error received:", err.message);
-        toast.error(err.message);
-    }); 
-    }
+  axios
+    .post(
+      "http://localhost:4000/register",
+      { email, username, password },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    )
+    .then((res) => {
+      console.log("Response received: ", res);
+      console.log(res.data);
+      toast.success(res);
+      navigate("/dashboard");
+    })
+    .catch((err) => {
+      console.log("Error received:", err.message);
+      toast.error(err.message);
+    });
+}
--- server/index.js
+++ server/index.js
@@ -8,45 +8,47 @@ const database = [];
 //generate random string id
 const generateID = () => Math.random().toString(36).substring(2, 10);
 
-app.use(urlencoded({extended: true}));
+app.use(urlencoded({ extended: true }));
 app.use(express.json());
+app.use((res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+});
 
 // http get api to get user data in string format.
-app.get("/api",(req, res) => {
-    res.json({
-        message: "hello world",
+app.get("/api", (req, res) => {
+  res.json({
+    message: "hello world",
+  });
+});
+
+app.post("/register", (req, res) => {
+  const { username, email, password } = req.body;
+  console.log(req.body);
+
+  //check if user exist
+  let result = database.filter(
+    (user) => user.email === email || user.username === username
+  );
+
+  //creates the user's data structure on the server
+  if (result.length === 0) {
+    database.push({
+      id: generateID(),
+      username,
+      password,
+      email,
+      timezone: {},
+      schedule: [],
     });
+    return res.json({ message: "Account created succesfully!" });
+  }
+  res.json({ error_message: "User already exists!" });
 });
 
-
-app.get("/register", (req, res) => {
-    //CORS policy.
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-
-    const { username, email, password } = req.body;
-    console.log(req.body);
-    
-    //check if user exist
-    let result = database.filter(
-        (user) => user.email === email || user.username === username
-    );
-
-    //creates the user's data structure on the server
-    if (result.length === 0) {
-        database.push({
-            id: generateID(),
-            username,
-            password,
-            email,
-            timezone: {},
-            schedule:[],
-        });
-        return res.json({ message: "Account created succesfully!" });
-    }
-    res.json({ error_message: "User already exists!" });
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 });
-
-app.listen(PORT, ()=>{
-    console.log(`Server listening on ${PORT}`);
-})
