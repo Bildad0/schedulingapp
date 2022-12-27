78efbe076561a7fb4328cdbb468033668ceabeea
BildadOwuor <bildadowuor@gmail.com>
2022-12-27 16:07:04 +0300

    updates and fixes

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,57 +1,62 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-
+import "../signup/Signup.css";
 
 const Login = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
-        if (username.trim() && password.trim()) {
-            e.preventDefault();
-            console.log({ username, password });
-            setPassword("");
-            setUsername("");
-        }
-    };
+  const handleSubmit = (e) => {
+    if (username.trim() && password.trim()) {
+      e.preventDefault();
+      console.log({ username, password });
+      setPassword("");
+      setUsername("");
+    }
+  };
 
-    return (
-        <main className='login'>
-            <form className="login_form" onSubmit={handleSubmit}>
-                <h2 className="login_title"> Log into your account</h2>
-                <label htmlFor="username">User Name</label>
-                <input
-                    id="username"
-                    name="username"
-                    type='text'
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    className='username'
-                />
-                <label htmlFor="password">Password</label>
-                <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    className="password"
-                />
-                <button className="loginButton">LOG IN</button>
-                <p style={{ textAlign: "center", margibTop: '30px' }}>
-                    Don't have an account? {" "}
-                    <Link className='link' to="/register">
-                        Create one
-                    </Link>
-
-                </p>
-
-            </form>
-
-        </main>
-
-    )
-}
+  return (
+    <main className="login">
+      <form className="login_form" onSubmit={handleSubmit}>
+        <h2 className="login_title"> Log into your account</h2>
+        <div className="form_items">
+          <label htmlFor="username" className="label">
+            User Name
+          </label>
+          <input
+            id="username"
+            name="username"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="textinput"
+          />
+          <label htmlFor="password" className="label">
+            Password
+          </label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="textinput"
+          />
+        </div>
+        <button className="loginButton">LOG IN</button>
+        <p
+          style={{ textAlign: "center", margibTop: "30px" }}
+          className="question"
+        >
+          Don't have an account?{" "}
+          <Link className="link" to="/register">
+            Create one
+          </Link>
+        </p>
+      </form>
+    </main>
+  );
+};
 
 export default Login;
--- client/src/components/signup/Signup.js
+++ client/src/components/signup/Signup.js
@@ -4,68 +4,79 @@ import { handleRegister } from "../../utils/resource";
 import "./Signup.css";
 
 const Signup = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [email, setEmail] = useState("");
-    const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (username.trim() && password.trim() && email.trim()) {
-            handleRegister(email, username, password, navigate);
-            //console.log(email, username, password);
-            setEmail("");
-            setUsername("");
-            setPassword("");
-        }
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username.trim() && password.trim() && email.trim()) {
+      handleRegister(email, username, password, navigate);
+      //console.log(email, username, password);
+      setEmail("");
+      setUsername("");
+      setPassword("");
+    }
+  };
 
-    return (
-        <main className="signup">
-            <form className="signup_form" onSubmit={handleSubmit}>
-                <h2 className="signup_title"> Create an Account</h2>
-                <div className="form_items">
-                <label htmlFor="email"> Email Address</label>
-                <input
-                    id="email"
-                    name="email"
-                    className="emailinput"
-                    type='email'
-                    required
-                    value={email}
-                    onChange={(e)=> setEmail(e.target.value)}
-                />
-                <label htmlFor="username">Username</label>
-                <input
-                    id="username"
-                    name="username"
-                    required
-                    value={username}
-                    type="text"
-                    onChange={(e)=> setUsername(e.target.value)}
-                />
-                <label htmlFor="password">Password</label>
-                <input
-                    id="password"
-                    name="password"
-                    required
-                    minLength={6}
-                    value={password}
-                    type="password"
-                    onChange={(e)=> setPassword(e.target.value)}
-                />
-                </div>
-                <button className="signupButton">REGISTER</button>
-                <p style={{ textAlign: "center", marginTop: "30px" }}>
-                    Already have an account? {""}
-                    <Link className="link" to='/'>
-                        Sign in
-                    </Link>
-                </p>
-            </form>
-            
-      </main>  
-    );
-}
+  return (
+    <main className="signup">
+      <form className="signup_form" onSubmit={handleSubmit}>
+        <h2 className="signup_title"> Create an Account</h2>
+        <div className="form_items">
+          <label htmlFor="email" className="label">
+            {" "}
+            Email Address
+          </label>
+          <input
+            id="email"
+            name="email"
+            className="textinput"
+            type="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="username" className="label">
+            Username
+          </label>
+          <input
+            id="username"
+            name="username"
+            className="textinput"
+            required
+            value={username}
+            type="text"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <label htmlFor="password" className="label">
+            Password
+          </label>
+          <input
+            id="password"
+            name="password"
+            className="textinput"
+            required
+            minLength={6}
+            value={password}
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button className="signupButton">REGISTER</button>
+        <p
+          style={{ textAlign: "center", marginTop: "30px" }}
+          className="question"
+        >
+          Already have an account? {""}
+          <Link className="link" to="/">
+            Sign in
+          </Link>
+        </p>
+      </form>
+    </main>
+  );
+};
 
 export default Signup;
