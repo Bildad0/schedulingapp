cdc29bd66b1b59c28d959a7e73904b65702043be
BildadOwuor <bildadowuor@gmail.com>
2023-03-01 11:53:10 +0300

    updated get all users

--- api/route/users.js
+++ api/route/users.js
@@ -6,21 +6,36 @@ const User = require("../models/userModel");
 mongoose.set("strictQuery", true);
 //get user
 
-userRouter.get("/profile", async (req, res) => {
+userRouter.get("/profile/:id", async (req, res) => {
   try {
-    const user = await User.findById(req.params.id)
+    const user = await User.findById({ _id: req.params.id })
       .populate("email")
       .populate("username")
       .populate("timezone")
       .populate("schedule");
-
-    if (user) {
+    if (user != null) {
       res.status(200).json(user);
     } else {
       res.status(404).json({ message: "User not found" });
     }
   } catch (error) {
     res.status(500).json(error.message);
+  }
+});
+
+userRouter.get("/all", async (req, res) => {
+  const query = req.query.new;
+  try {
+    const users = query
+      ? await User.find()
+          .sort({
+            _id: -1,
+          })
+          .limit(5)
+      : await User.find();
+    res.status(200).json({ users });
+  } catch (error) {
+    res.status(500).json({ message: "No Users" });
   }
 });
 
