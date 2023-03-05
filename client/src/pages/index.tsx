e1ea87708eab8699bb511bebb1a2244b9a12882f
BildadOwuor <bildadowuor@gmail.com>
2023-03-05 18:32:30 +0300

    updates and fixes

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -12,8 +12,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="calendar.png" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.nav_bar}>
+      
+      <nav className={styles.nav_bar}>
           <div className={styles.nav_bar_content}>
             <h1 className={styles.title}>My Scheduler</h1>
             <div className={styles.navigation}>
@@ -31,23 +31,25 @@ export default function Home() {
                 <li className={styles.login}>LOGIN</li>
               </ul>
             </div>
-        </div>
-        
+        </nav>
+
+      <main className={styles.main}>        
         <div className={styles.main_body}>
           <div className={styles.main_body_info}>
             <div className={styles.main_body_info_content}>
               <p className={styles.catch_phrase}>SIMPLIFY YOUR SCHEDULING</p>
               <h1 className={styles.main_body_info_title}>Automate <br />Your Appointments.</h1>
               <p className={styles.main_body_info_description}>
-                If you diligently track time, you’re also much more aware of how you <br />
-                spend the given hours in a day and set priorities. You can use  <br />project
-                time tracking tools such as this to make sure tracking time is intuitive,
-                hassle-free, and actually useful.
+                If you diligently track time, you’re also much more aware of how you spend <br />
+                the given hours in a day and set priorities. You can use project time tracking  <br />
+                tools such as My Scheduler to make sure tracking time is intuitive,hassle-free, and actually useful.
               </p>
               <button className={styles.trial_button}><h1>Start Free Trial</h1></button>
           </div>
           </div>
-          <div className={styles.svg}> <Image src="/calendar.jpg" alt={'Calender svg'} width="500" height="500" className={styles.image }/></div>
+          <div className={styles.svg}>
+            <Image src="/calendar.jpg" alt={'Calender svg'} width="500" height="500" className={styles.image} />
+          </div>
         </div>
       </main>
       <div className={styles.footer} >
@@ -58,15 +60,14 @@ export default function Home() {
           </div>
           <div className={styles.about_developer}>
             <h1>Our Team</h1>
-            <p>We are a team of software developers <br />
-              dedicated to deliver quality Mobile and web applications <br />
-              to our esteamed customers.
+            <p>We are a team of software developers dedicated to deliver quality <br />
+              Mobile and web applications to our esteamed customers.
             </p>
             <div className={styles.our_location}>
               <h1>Location</h1>
-              <p> We are located in <i>Nairobi, Kenya</i> <br />
+              <p> We are located in <i>Nairobi, Kenya.</i> <br />
                 You can reach us through email: <a href="mailto:bildadowuor@gmail.com"> Bildadowuor@gmail.com</a><br />
-                Or Call <a href="tel:+2547-005-4820">0700524820</a>
+                Or Call <a href="tel:+2547-005-4820">0700524820.</a>
               </p>              
             </div>
           </div>
@@ -78,9 +79,10 @@ export default function Home() {
             </ul>
           </div>
         </div>
+        <p><a href="">Get our Mobile APP</a></p>
       </div>
       <div className={styles.footer_bottom}>
-      <p className={styles.my_slogan}>Made with <span> &hearts;</span> by Bildad</p>
+      <p className={styles.my_slogan}>Made with <span> &hearts;</span> by  <a href="https://github.com/Bildad0" target="_blank" rel="noopener noreferrer"> Bildad</a></p>
         <p className={styles.copyright}>© 2023 The Rookies Co. All rights reserved.</p>
         </div>
     </>
