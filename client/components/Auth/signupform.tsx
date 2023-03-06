793ea65cd955ca866381cec2a4d9014c04c206be
BildadOwuor <bildadowuor@gmail.com>
2023-03-06 13:27:24 +0300

    updates and fixes

--- client/components/Auth/signupform.tsx
+++ client/components/Auth/signupform.tsx
@@ -1,25 +1,17 @@
-import style from '@/styles/login.module.css'
-import styles from '@/styles/Home.module.css'
-import {useFormik } from 'formik'
-import Head from 'next/head'
-import { useState } from 'react'
+import styles from '@/styles/signup.module.css'
+import { useFormik } from 'formik'
+import Image from 'next/image'
+
+
 
 const SignUpForm =()=> {
-    const [value, setValues] = useState({});
-    
-    const handleChange = (e:any) => {
-        setValues(preValues => ({
-            ...preValues,
-            [e.target.name]: e.target.value
-        }))
-    }
     const validate = (values: any) => {
         const errors = {
-            username: values.username,
-            password: values.password,
-            email: values.email,
-            timezone: values.timemezone,
-            confirmPassword: values.confirmPassword
+            username:"",
+            password:"",
+            email:"",
+            timezone:"",
+            confirmPassword:""
         };
       
         if (!values.username) {
@@ -39,7 +31,7 @@ const SignUpForm =()=> {
             errors.password = 'Must be 6 characters or more';
         }
         if (!values.confirmPassword) {
-            errors.confirmPassword = 'Required';
+            errors.confirmPassword =  'Required';
         } else if (values.password != values.confirmPassword) {
             errors.confirmPassword = 'Passwords are not the same';
         }
@@ -51,6 +43,11 @@ const SignUpForm =()=> {
         }
         return errors;
     }
+
+    // const submitValues = (values: any) => {
+    //      //TODO: Make API function that takes values and submit to the backend
+    //     console.log('Submitted');
+    // }
     const formik = useFormik({
         initialValues: {
             email: '',
@@ -60,23 +57,24 @@ const SignUpForm =()=> {
             confirmPassword: '',
         },
         validate,
-        onSubmit: (values) => {
-            //TODO: Make API function that takes values and submit to the backend
-            console.log(values);
+        onSubmit: (values)=>{
+            console.log("submited")
         },
+           
+       
     });
     return (
         <>
-         <Head>
-            <title>My Scheduler || Sign Up</title>
-            <meta name="description" content="A web app used to book appointments and schedule activities." />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="icon" href="calendar.png" />
-           </Head>
-           
-
-        <form onSubmit={formik.handleSubmit}>
-       <label htmlFor="email">Email Address</label>
+            <form onSubmit={formik.handleSubmit} className={styles.form}>
+                <div className={styles.form_upper}>
+                    <div className={styles.form_upper_image}>
+                        <Image src={"/calendar.png"} height="40" width="40" alt={'Logo'} />
+                    </div>
+                </div>
+                <div className={styles.form_title}>
+                        <h1>Sign Up</h1>
+                    </div>
+       <label htmlFor="email">Email Address:</label>
        <input
          id="email"
          name="email"
@@ -84,8 +82,8 @@ const SignUpForm =()=> {
          onChange={formik.handleChange}
          value={formik.values.email}
         />
-     {formik.touched.email && formik.errors.email ? (<div>{ formik.errors.email}</div>):null}
-      <label htmlFor="username">Username</label>
+     {formik.touched.email && formik.errors.email ? (<div className={styles.error}>{ formik.errors.email}</div>):null}
+      <label htmlFor="username">Username:</label>
        <input
          id="username"
          name="username"
@@ -94,8 +92,8 @@ const SignUpForm =()=> {
         onBlur={formik.handleBlur}        
          value={formik.values.username}
                 />
-                {formik.touched.username && formik.errors.username ? (<div>{ formik.errors.username}</div>):null}
-        <label htmlFor="timezone">Timezone</label>
+                {formik.touched.username && formik.errors.username ? (<div className={styles.error}>{ formik.errors.username}</div>):null}
+        <label htmlFor="timezone">Timezone:</label>
        <input
          id="timezone"
          name="timezone"
@@ -104,8 +102,8 @@ const SignUpForm =()=> {
         onBlur={formik.handleBlur} 
          value={formik.values.timezone}
                 />
-                 {formik.touched.timezone && formik.errors.timezone ? (<div>{ formik.errors.timezone}</div>):null}
-     <label htmlFor="password">Password</label>
+                 {formik.touched.timezone && formik.errors.timezone ? (<div className={styles.error}>{ formik.errors.timezone}</div>):null}
+     <label htmlFor="password">Password:</label>
        <input
          id="password"
          name="password"
@@ -114,20 +112,18 @@ const SignUpForm =()=> {
                     value={formik.values.password}
                     onBlur={formik.handleBlur} 
                 />
-                {formik.touched.password && formik.errors.password ? (<div>{ formik.errors.password}</div>):null}
-      <label htmlFor="confirmPassword">Confirm Password</label>
+                {formik.touched.password && formik.errors.password ? (<div className={styles.error}>{ formik.errors.password}</div>):null}
+      <label htmlFor="confirmPassword">Confirm Password:</label>
        <input
          id="confirmPassword"
          name="confirmPassword"
          type="password"
          onChange={formik.handleChange}
-                    value={formik.values.confirmPassword}
-                    onBlur={formik.handleBlur} 
-                />
-                {formik.touched.confirmPassword && formik.errors.confirmPassword ? (<div>{ formik.errors.confirmPassword}</div>):null}
-       <button type="submit">Submit</button>
+         value={formik.values.confirmPassword}
+         onBlur={formik.handleBlur} 
+        />
+       <button type="submit" value="submit">Register</button>
      </form>
-    
     </>
     )
 }
--- client/src/pages/auth/signup.tsx
+++ client/src/pages/auth/signup.tsx
@@ -1,12 +1,22 @@
-import type { NextPage } from 'next'
-import Header from '../../../components/Header/header' 
+import type { NextPage } from 'next' 
 import SignUpForm from '@/../components/Auth/signupform'
+import Head from 'next/head'
+import styles from '@/styles/signup.module.css'
 
 const SignUp: NextPage = () => {
     return (
         <>
-            <Header />
-            <SignUpForm/>
+              <Head>
+            <title>My Scheduler || Sign Up</title>
+            <meta name="description" content="A web app used to book appointments and schedule activities." />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="calendar.png" />
+           </Head>
+
+            <main className={styles.main}>
+                <SignUpForm />
+            </main>
+            
     </>
     )
 }
